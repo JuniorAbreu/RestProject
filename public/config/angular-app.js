3d7ffe97f81207c226d5a9d7e15d59d902cc945b
Waldemario Junior <>
2020-02-19 15:41:05 -0300

    remove useless code

--- public/config/angular-app.js
+++ public/config/angular-app.js
@@ -3,16 +3,12 @@
 var app = angular.module('RestApp', ['ngRoute']);
 
 app.config(function($routeProvider) {
-	var resolveProjects = {
-		projects: function (Projects) {
-			return Projects.fetch();
-		}
-	};
 
 	$routeProvider.when('/home', {
 		controller:'HomeController',
 		templateUrl:'src/home/home.html'
 	}).otherwise({ redirectTo: '/home'});
+	
 });
 
 app.run();
