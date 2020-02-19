905469269c0897d13a5fcec60de244fda8e5732e
Waldemario Junior <>
2020-02-19 14:38:08 -0300

    add test

--- server/index.js
+++ server/index.js
@@ -1,9 +1,7 @@
 module.exports = function(app){
 
-	var https 		= require('https');
 	var bodyParser 	= require('body-parser');
 	var session 	= require('express-session');
-	var fs 			= require('fs');
 	var cors 		= require('cors');
     var port = process.env.PORT || 8080;
 
@@ -11,12 +9,9 @@ module.exports = function(app){
 
 	app.use(bodyParser.json({limit: '100mb'}));
 	app.use(bodyParser.urlencoded({ extended: true, limit: '100mb', parameterLimit: 50000 }));
-
-	// START OF CHANGE
 	app.use(session({resave: 'true', saveUninitialized: 'true' , secret: 'keyboard cat'}));
 
-	// Disable http - only https is enabled
-	app.listen(8080, function() {
+	app.listen(port, function() {
 		console.info("To view your app, open this link in your browser: http://localhost:" + port);
 	});
 
