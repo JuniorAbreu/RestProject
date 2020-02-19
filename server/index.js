88207620bfcd1099f9f87433b8ea477ec77fe129
Waldemario Junior <>
2020-02-19 00:01:14 -0300

    remove data

--- server/index.js
+++ server/index.js
@@ -5,7 +5,7 @@ module.exports = function(app){
 	var session 	= require('express-session');
 	var fs 			= require('fs');
 	var cors 		= require('cors');
-    // var port = 9454;
+    var port = process.env.PORT || 8080;
 
 	app.use(cors());
 
@@ -16,25 +16,8 @@ module.exports = function(app){
 	app.use(session({resave: 'true', saveUninitialized: 'true' , secret: 'keyboard cat'}));
 
 	// Disable http - only https is enabled
-	// app.listen(port, function() {
-	// 	console.info("To view your app, open this link in your browser: http://localhost:" + port);
-	// });
-
-    // running LOCALLY
-    app.set('port_https', 9443);
-    app.all('*', function(req, res, next) {
-        if (req.secure) {
-            return next();
-        };
-        res.redirect('https://' + req.hostname + ':' + app.get('port_https') + req.url);
-    });
-
-    // Setup HTTPS
-    var options = {
-        key: fs.readFileSync(__dirname + '/certs/key.pem'),
-        cert: fs.readFileSync(__dirname + '/certs/cert.pem')
-    };
-    var secureServer = https.createServer(options, app).listen(app.get('port_https'));
-    console.info("To view your app, open this link in your browser: https://localhost:" + app.get('port_https'));
+	app.listen(8080, function() {
+		console.info("To view your app, open this link in your browser: http://localhost:" + port);
+	});
 
 }
